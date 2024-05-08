77acd6085a8196552b058bc3b0c3067b38e45ea4
iwas108 <64204327+iwas108@users.noreply.github.com>
2024-05-08 20:00:25 +0800

    wip

---
+++ test/Vanilla/include/params.h
@@ -0,0 +1,11 @@
+#ifndef PARAMS_H
+#define PARAMS_H
+
+#define SERIAL_BAUD_RATE 115200
+
+int tbPort = 8883;
+constexpr char tbAddr[] PROGMEM = "prita.undiknas.ac.id";
+constexpr char spiffsBinUrl[] PROGMEM = "http://prita.undiknas.ac.id/cdn/firmware/sudarsan.spiffs.bin";
+constexpr char model[] PROGMEM = "Generic";
+
+#endif
---
+++ test/Vanilla/src/main.h
@@ -0,0 +1,6 @@
+#include <Arduino.h>
+#include "params.h"
+#include "secret.h"
+#include "UdawaLogger.h"
+#include "UdawaSerialLogger.h"
+#include "Udawa.h"
